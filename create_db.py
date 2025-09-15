5985500f9990982c4b1c0f4d6d184316e5263994
A01753453 <A01753453@tec.mx>
2025-09-14 21:19:42 -0600

    Delete create_db.py

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from app.db import Base, engine
-from app import models
-
-def main():
-    Base.metadata.create_all(bind=engine)
-    print("Tablas creadas.")
-
-if __name__ == "__main__":
-    main()
